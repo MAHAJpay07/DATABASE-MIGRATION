1b9cf6a8d958b226a782b828460ac51dfd6f792a
Payal Mahajan <payalmahajan1304@gmail.com>
2025-04-08 20:44:32 +0530

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -1,1 +1,33 @@
+--migration task from Mysql To postgreSQL 
+
+--export from MySQL
+ SELECT * FROM employees INTO OUTFILE '/tmp/employees.csv'
+ FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+ LINES TERMINATED BY '\n';
+
+--creating table in postgreSQL
+  CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  position VARCHAR(100),
+  salary NUMERIC(10,2)
+);
+
+--import CSV into postgreSQL
+COPY employees(name, position, salary)
+FROM '/tmp/employees.csv'
+DELIMITER ','
+CSV;
+
+--verifying the row count / or can be called verifying data integrity
+ SELECT COUNT(*) FROM employees;
+
+--dislpay the imported data 
+ SELECT * FROM employees;
+
+
+
+
+
+
 
